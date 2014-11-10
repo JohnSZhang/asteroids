72fdc2494f945bc782e05cc9fc6aa6f4da143395
John Zhang <john.shimin.zhang@gmail.com>
2014-11-09 23:25:19 -0500

    add nodejs server

---
+++ README.md
@@ -0,0 +1,1 @@
+This Is A Project That Started As An Asteroids Clone, Now... I'm not so sure anymore. 
---
+++ server.js
@@ -0,0 +1,39 @@
+var http = require('http');
+var mime = require('mime');
+var fs = require('fs');
+var path = require('path');
+var cache = {};
+
+function staticServer(response, cache, filePath) {
+  if (cache[filePath]) {
+    getFile(response, path, cahce[filePath]);
+  } else {
+    fs.readFile(filePath, function(status, data){
+      cache[filePath] = data;
+      getFile(response, filePath, data)
+    })
+  }
+
+}
+
+function getFile(response, filePath, content) {
+  response.writeHead(200,
+    {'Content-Type': mime.lookup(
+          path.basename(filePath)
+          )
+    });
+  response.end(content);
+}
+
+
+http.createServer(function (req, res) {
+  var filePath = false;
+  if(req.url === '/') {
+    filePath = 'index.html';
+  } else {
+    filePath = req.url
+  }
+  var fullPath = './' + filePath;
+  staticServer(res, cache, fullPath)
+}).listen(3000);
+console.log('Server running at http://localhost:3000/')
